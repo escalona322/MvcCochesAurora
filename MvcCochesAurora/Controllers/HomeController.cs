feb816442b8a15a2c80403102b5472a8c3ad170e
Jose Antonio Lozano <escalona322@gmail.com>
2022-04-29 11:30:45 +0200

    Add project files.

---
+++ MvcCochesAurora/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcCochesAurora.Models;
+using MvcCochesAurora.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCochesAurora.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private RepositoryCoches repo;
+
+       
+        public HomeController(ILogger<HomeController> logger, RepositoryCoches repo)
+        {
+            _logger = logger;
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaCoches()
+        {
+            List<Coche> coches = this.repo.GetCoches();
+            return View(coches);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Coche coche)
+        {
+            this.repo.InsertCoche(coche.IdCoche, coche.Marca, coche.Modelo, coche.Conductor, coche.Imagen);
+            return RedirectToAction("ListaCoches");
+        }
+
+        public IActionResult Details(int idcoche)
+        {
+            Coche coche = this.repo.FindCoche(idcoche);
+            return View(coche);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcCochesAurora/Repositories/RepositoryCoches.cs
@@ -0,0 +1,43 @@
+﻿using MvcCochesAurora.Data;
+using MvcCochesAurora.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCochesAurora.Repositories
+{
+    public class RepositoryCoches
+    { 
+     private CochesContext context;
+
+    public RepositoryCoches(CochesContext context)
+    {
+        this.context = context;
+    }
+
+    public List<Coche> GetCoches()
+    {
+        return this.context.Coches.ToList();
+    }
+
+    public Coche FindCoche(int id)
+    {
+        return this.context.Coches.SingleOrDefault(x => x.IdCoche == id);
+    }
+
+    public void InsertCoche(int id, string marca, string modelo, string conductor, string imagen)
+    {
+            Coche p = new Coche
+            {
+            IdCoche = id,
+            Marca = marca,
+            Modelo = modelo,
+            Conductor = conductor,
+            Imagen = imagen
+        };
+        this.context.Coches.Add(p);
+        this.context.SaveChanges();
+    }
+}
+}
